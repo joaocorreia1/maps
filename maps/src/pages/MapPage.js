878c95a3b047ea59010e3baba1087cd0a7e9030a
João Pedro Correia <jpedrocbarros@gmail.com>
2022-09-08 17:02:49 -0300

    map implemented

---
+++ maps/src/pages/MapPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+import "./MapPage.css";
+
+const MapPage = () => {
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+  return (
+    <div className="map">
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          center={{
+            lat: -23.567914149043254,
+            lng: -46.70148579705192,
+          }}
+          zoom={15}
+        ></GoogleMap>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default MapPage;
